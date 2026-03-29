4aee7a09e8c3c86c13cdc6525c2502f74ec97525
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2026-03-29 20:32:28 +0500

    edit form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,9 +4,9 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
- 
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -14,8 +14,9 @@ const FormSchema = z.object({
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
- 
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function createInvoice(formData: FormData) {
   const {customerId, amount, status} = CreateInvoice.parse( {
@@ -34,4 +35,23 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
+}
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,9 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { UpdateInvoice } from './buttons';
+import { updateInvoice } from '@/app/lib/actions';
+
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +20,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+const UpdateInvoicesWithId = updateInvoice.bind(null, invoice.id)
   return (
-    <form>
+    <form action={UpdateInvoicesWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
